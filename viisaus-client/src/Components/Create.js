0b8e37518e889eb2278525d352091f0ee62e1e13
SariPaavilainen <sari.paavilainen@iki.fi>
2018-08-13 11:40:55 +0300

    Create-napin säätöä forkkausta varten

--- viisaus-client/src/Components/Create.js
+++ viisaus-client/src/Components/Create.js
@@ -3,12 +3,12 @@ import { addNewUser } from '../ServiceDesk';
 import './Create.css';
 
 class Create extends Component {
-    state={
-        createFailed: false,
-    }
+  state = {
+    createFailed: false,
+  }
 
   constructor(props) {
-    super(props);    
+    super(props);
     this.state = {
       name: '',
       password: '',
@@ -20,15 +20,15 @@ class Create extends Component {
   passwordChanged = (e) => this.setState({ password: e.target.value })
 
   addUser = (state) => {
-    addNewUser(state, function (response){
+    addNewUser(state, function (response) {
       console.log(response);
-      if(response === 401){
-          alert("Try again, Nickname is not available!")
+      if (response === 401) {
+        alert("Try again, Nickname is not available!")
       }
-      else{
+      else {
         this.props.backToLogin();
       }
-      
+
     }.bind(this));
   }
 
@@ -37,16 +37,16 @@ class Create extends Component {
     this.addUser(this.state);
     this.setState({ name: '', password: '' });
   }
- 
+
   render() {
     return (
       <div className="create" id="alert">
-       
+
         <form onSubmit={this.CreateUser}>
-          <input className="teksti" placeholder="Nickname" value={this.state.name} onChange={this.nameChanged} required minLength="3" maxLength="50"/>  <br />
-          <input className="teksti" placeholder="Password" type="password" value={this.state.password} onChange={this.passwordChanged} required minLength="6" maxLength="50"/>
-          <input className="nappi2" type="submit" value="Create account" onClick={this.handleClick}/>
-         
+          <input className="teksti" placeholder="Username" value={this.state.name} onChange={this.nameChanged} required minLength="3" maxLength="50" />  <br />
+          <input className="teksti" placeholder="Password" type="password" value={this.state.password} onChange={this.passwordChanged} required minLength="6" maxLength="50" />
+          <input className="nappi2" type="submit" value="Create account" onClick={this.handleClick} />
+
         </form>
       </div>
     );
